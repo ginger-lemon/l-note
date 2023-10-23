2ac83ab01c3b7e6db85353ef4aeff5d72b418efe
gingerlemon <ycjeanlee.cdng@gmail.com>
2023-10-23 15:28:20 +0800

    重構 App 程式碼

--- src/App.js
+++ src/App.js
@@ -5,27 +5,21 @@ import Edit from "./pages/Edit/Edit";
 import Note from "./pages/Note/Note";
 import Error from "./pages/Error/Error";
 
-import NoteMode from "./pages/NoteMode";
-import { CSSReset} from "./styles/CSS-Reset";
-import { Route, Routes, useParams } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { NoteDataContextProvider } from "./Hooks/NoteContext";
-import ErrorMode from "./pages/ErrorMode";
 
-export default function App() {
-    // TO DO: 處理 noteID 的問題，先用 testNote 當測試路徑
-    const { id } = useParams();
-
+const App = () => {
     return (
         <>  
-            {/* <CSSReset /> */}
             <NoteDataContextProvider>
             <Routes>
                 <Route path="/" element={<Edit />}/>
                 <Route path="/:id" element={<Note/>}/>
-                <Route path="/error" element={<Error />} />
+                <Route path="*" element={<Error />} />
             </Routes>
             </NoteDataContextProvider>
         </>
     );
 }
 
+export default App
